16004518b47ea7ce98ce93a1a52cbb91e2cfc1b4
Olga <kindzyadzyu@gmail.com>
2020-01-22 13:29:36 +0200

    UI: add show/hide button

--- LogInView/LoginViewController.swift
+++ LogInView/LoginViewController.swift
@@ -13,7 +13,6 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var textfieldStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
