083a0ae13812d02b71d7c9a8ab761251e1040642
Olga <kindzyadzyu@gmail.com>
2020-01-24 21:15:53 +0200

    check email for containing "." in domain name

--- LogInView/Helper/Extensions.swift
+++ LogInView/Helper/Extensions.swift
@@ -20,16 +20,4 @@ extension String {
 }
 
 
-//MARK: - Text field padding (ext.)
-extension UITextField {
-    func setLeftPaddingPoints(_ amount:CGFloat){
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
-        self.leftView = paddingView
-        self.leftViewMode = .always
-    }
-    func setRightPaddingPoints(_ amount:CGFloat) {
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
-        self.rightView = paddingView
-        self.rightViewMode = .always
-    }
-}
+
